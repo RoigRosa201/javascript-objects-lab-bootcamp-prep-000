eda1df9e9ed1172bad58c673159703697a178236
Roigrosa <roigrosa88@gmail.com>
2018-04-14 06:19:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
   delete object[key]
   return object
-  delete object[key];
-  return object
+}
+}
+delete object[key];
+return object
 }
